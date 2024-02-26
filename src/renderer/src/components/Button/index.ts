18aa2f3f3b3699fdfdf24c8306009be4b59ac321
memosama <carlosthegamingwizard@gmail.com>
2024-02-26 16:26:36 -0500

    restart

--- src/renderer/src/components/Button/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './ActionButton'
-export * from './DeleteNoteButton'
-export * from './NewNoteButton'
--- src/shared/constants.ts
+++
@@ -1,5 +0,0 @@
-export const appDirectoryName = 'NoteMark'
-export const fileEncoding = 'utf8'
-
-export const autoSavingTime = 3000
-export const welcomeNoteFilename = 'Welcome.md'
