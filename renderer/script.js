a1c2fa2efa8ad5f89039feffebe8fd33da21ec7d
memosama <carlosthegamingwizard@gmail.com>
2024-02-29 08:50:20 -0500

    update css and js

--- renderer/script.js
+++ renderer/script.js
@@ -1,18 +1,39 @@
 const { ipcRenderer } = require('electron');
 
-document.addEventListener('DOMContentLoaded', async () => {
-    let notes = await ipcRenderer.invoke('get_data');
-    console.log(notes);
-});
+let notes = [];
 
-document.getElementById('btn').onclick = () => {
-    let title = document.getElementById('title').value;
-    let noteText = document.getElementById('note').value;
+let list = document.getElementById("list")
 
-    if (title.trim() !== "" && noteText.trim() !== "") {
-        let note = { title, note: noteText };
-        ipcRenderer.send('save_note', note);
-    } else {
+function loadNotes(){
+    list.innerHTML = "";
+    notes.forEach((note, idx) => {
+        list.innerHTML += `
+            <div class="list-ele">
+                <h1>${idx} ${note.title} <h1>
+                <p> ${note.note} <p>
+            <div>
+        `
+    });
+}
+
+window.onload = async () => {
+    notes = await ipcRenderer.invoke('get_data');
+    loadNotes();  
+};
+
+btn.onclick = () => {
+    if (title !== "" && note !== ""){
+        let _note = {
+            title : title.value,
+            note : note.value,
+        };
+
+        notes.push(_note);
+        loadNotes();
+
+
+        ipcRenderer.send('save_note', _note);
+    }else {
         window.alert("Please fill in all the fields and try again.");
     }
 };
